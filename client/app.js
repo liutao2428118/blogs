6c5f099d8048c9d4defe95c1d578eb8ae494a382
tao.lt <liutao2428118@163.com>
2020-08-16 22:18:12 +0800

    添加github-markdown-css

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@ import createRouter from './router/index'
 import createStore from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import 'github-markdown-css/github-markdown.css'
 
 import './assets/styles/global.styl'
 import App from './App.vue'
@@ -10,14 +11,14 @@ import App from './App.vue'
 Vue.use(ElementUI);
 
 export default () => {
-  const router = createRouter()
-  const store = createStore()
+    const router = createRouter()
+    const store = createStore()
 
-  const app = new Vue({
-    router,
-    store,
-    render: h => h(App)
-  })
+    const app = new Vue({
+        router,
+        store,
+        render: h => h(App)
+    })
 
-  return { app, router, store }
+    return { app, router, store }
 }
--- client/views/article/article.vue
+++ client/views/article/article.vue
@@ -1,59 +1,74 @@
 <template>
-	<div>
-		<el-row class="main" type="flex" justify="center">
-			<el-col :span="16" class="col">
-				<div class="artcle-info" :style="{backgroundImage: `url('${articleOne.imageUrl}')`}">
-					<h2 class="text-center"><strong>{{articleOne.title}}</strong></h2>
-					<!-- 描述：文章信息 -->
-					<div class="text-center timeAndView">
-						<span class="article-time">
-							<i class="el-icon-time"></i>
-							发表于：<span>{{articleOne.createdAt | dateFrm}}</span>
-						</span>
-						&nbsp;|&nbsp;
-						<span class="article-views">
-							<i class="el-icon-view"></i>
-							阅读量：<span>{{articleOne.pageview}}</span>万
-						</span>
-					</div>
-					<p class="abstract">
-						{{articleOne.outline}}
-					</p>
-				</div>
-				<hr />
+    <div>
+        <el-row class="main" type="flex" justify="center">
+            <el-col :span="16" class="col">
+                <div
+                    class="artcle-info"
+                    :style="{backgroundImage: `url('${articleOne.imageUrl}')`}"
+                >
+                    <h2 class="text-center">
+                        <strong>{{articleOne.title}}</strong>
+                    </h2>
+                    <!-- 描述：文章信息 -->
+                    <div class="text-center timeAndView">
+                        <span class="article-time">
+                            <i class="el-icon-time"></i>
+                            发表于：
+                            <span>{{articleOne.createdAt | dateFrm}}</span>
+                        </span>
+                        &nbsp;|&nbsp;
+                        <span class="article-views">
+                            <i class="el-icon-view"></i>
+                            阅读量：
+                            <span>{{articleOne.pageview}}</span>万
+                        </span>
+                    </div>
+                    <p class="abstract">{{articleOne.outline}}</p>
+                </div>
+                <hr />
 
                 <!-- 内容 -->
-				<div id="artcle-content" v-html="content" ></div>
+                <div class="markdown-body" v-html="content"></div>
 
-				<div id="statement">
-					<div class="item"></div>
-					<div class="item">
-						<a href="https://www.xiaoying.love">https://www.xiaoying.love</a>
-					</div>
-					<div class="item">本博客所有文章除特别声明外,转载请注明出处!</div>
-				</div>
+                <div id="statement">
+                    <div class="item"></div>
+                    <div class="item">
+                        <a href="https://www.xiaoying.love">https://www.xiaoying.love</a>
+                    </div>
+                    <div class="item">本博客所有文章除特别声明外,转载请注明出处!</div>
+                </div>
                 <div class="comments">
                     <div class="title">评论区</div>
                     <reply :articleId="$route.params.id" :reply="articleOne.reply"></reply>
                 </div>
-			</el-col>
-		</el-row>
-	</div>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
-import MarkdownIt  from 'markdown-it'
-import moment from "moment"
-import Reply from "../../components/reply/reply.vue"
+import MarkdownIt from "markdown-it";
+import moment from "moment";
+import Reply from "../../components/reply/reply.vue";
 
-const md = new MarkdownIt()
+const md = MarkdownIt({
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+            try {
+                return hljs.highlight(lang, str, true).value;
+            } catch (__) {}
+        }
+
+        return ""; // 使用额外的默认转义
+    }
+});
 
 export default {
     name: "artile",
     data() {
         return {
-            content: ''
+            content: "",
         };
     },
     created() {},
@@ -63,90 +78,105 @@ export default {
     },
     asyncData({ app, router, store }) {
         return Promise.all([
-            store.dispatch("fetchArticleDetails", app.$route.params.id)
-        ])
+            store.dispatch("fetchArticleDetails", app.$route.params.id),
+        ]);
     },
     computed: {
-       ...mapState([
-           "articleOne"
-       ])
+        ...mapState(["articleOne"]),
     },
     filters: {
         dateFrm(date) {
-            return moment(date).format("YYYY-MM-DD")
-        }
+            return moment(date).format("YYYY-MM-DD");
+        },
     },
-    methods: {
-        
-    },
+    methods: {},
 
     components: {
-       Reply
-    }
+        Reply,
+    },
 };
 </script>
 
 <style lang="stylus" scoped>
-.col
-    background-color #fff
-    padding 20px
-.artcle-info 
-    padding: 20px
-    margin-bottom: 40px
-    background-repeat: no-repeat
-    background-size: 100% 100%
-    .article-time,.article-views
-        color #eee
-    .abstract 
-        color: #ffffff
-        border-left: 3px solid #F56C6C
-        padding: 10px
-        background-color: rgba(126, 129, 135, 0.3)
+.col {
+    background-color: #fff;
+    padding: 20px;
+}
 
-    .timeAndView 
-        padding: 20px
-        line-height: 30px
-        font-size: 16px
-        color: #000
-#artcle-content
-    .blockquote
-        background-color: #f5f2f0
-        padding: 1em
-        margin: 2em 2em
-        font-family: Consolas,Monaco,"Andale Mono",monospace
-        border-radius: 1em
-        border-width: .2em
-        border-color: #e0dfcc
-        border-style: solid
-        text-shadow: 0 1px white
-        .has 
-            background: #f5f2f0
-            color: black
-            text-shadow: 0 1px white
-            font-family: monospace,Consolas,Monaco,"Andale Mono"
-            direction: ltr
-            text-align: left
-            white-space: pre
-            word-spacing: normal
-            font-size: 1.2em
-            tab-size: 4
-            hyphens: none
-            padding: 0em 0em .5em 0em
+.artcle-info {
+    padding: 20px;
+    margin-bottom: 40px;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+
+    .article-time, .article-views {
+        color: #eee;
+    }
+
+    .abstract {
+        color: #ffffff;
+        border-left: 3px solid #F56C6C;
+        padding: 10px;
+        background-color: rgba(126, 129, 135, 0.3);
+    }
+
+    .timeAndView {
+        padding: 20px;
+        line-height: 30px;
+        font-size: 16px;
+        color: #000;
+    }
+}
+
+#artcle-content {
+    .blockquote {
+        background-color: #f5f2f0;
+        padding: 1em;
+        margin: 2em 2em;
+        font-family: Consolas, Monaco, 'Andale Mono', monospace;
+        border-radius: 1em;
+        border-width: 0.2em;
+        border-color: #e0dfcc;
+        border-style: solid;
+        text-shadow: 0 1px white;
+
+        .has {
+            background: #f5f2f0;
+            color: black;
+            text-shadow: 0 1px white;
+            font-family: monospace, Consolas, Monaco, 'Andale Mono';
+            direction: ltr;
+            text-align: left;
+            white-space: pre;
+            word-spacing: normal;
+            font-size: 1.2em;
+            tab-size: 4;
+            hyphens: none;
+            padding: 0em 0em 0.5em 0em;
             // overflow: auto
-            font-size: 14px
+            font-size: 14px;
+        }
+    }
 
-    img
-        width: 100%
+    img {
+        width: 100%;
+    }
+}
 
-#statement 
-    border-left: 3px solid #F56C6C
-    padding: 20px
-    background-color: #EBEEF5
-.comments
-    padding 40px 0
-    .title
-        font-size 18px
-        font-weight 800
-        border-bottom 1px solid #ddd
-        padding-bottom: 10px 
+#statement {
+    border-left: 3px solid #F56C6C;
+    padding: 20px;
+    background-color: #EBEEF5;
+}
+
+.comments {
+    padding: 40px 0;
+
+    .title {
+        font-size: 18px;
+        font-weight: 800;
+        border-bottom: 1px solid #ddd;
+        padding-bottom: 10px;
+    }
+}
 </style>
