d8b3565c8ab0dc736689b7a7a72b57fa010cf995
tao.liu <466124202@qq.com>
2020-08-24 14:20:22 +0000

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  db: 'mongodb://localhost/blogs',
+  db: 'mongodb://127.0.0.1/blogs',
   qiniu: {
     accessKey: 'PrnH4meXiDDaBLBWVBJBeD-Fk2fmyZrCusGpXn2O',
     secretKey: 'DBssrRTDjZHAE9qIXaQdCxaFwhssDPl5IWKXBUwI'
