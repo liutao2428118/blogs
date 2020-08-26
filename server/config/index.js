5479d0de330716d0263db96e7a8306bfd938e482
tao.lt <liutao2428118@163.com>
2020-08-26 11:06:13 +0800

    开发与正式环境数据库连接区分

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  db: 'mongodb://mongo/blogs',
+  db: process.env.NODE_ENV === 'development' ? 'mongodb://localhost/blogs' : 'mongodb://mongo/blogs',
   qiniu: {
     accessKey: 'PrnH4meXiDDaBLBWVBJBeD-Fk2fmyZrCusGpXn2O',
     secretKey: 'DBssrRTDjZHAE9qIXaQdCxaFwhssDPl5IWKXBUwI'
--- server/middlewares/database.js
+++ server/middlewares/database.js
@@ -6,31 +6,38 @@ import config from '../config'
 const models = resolve(__dirname, '../database/schema')
 
 fs.readdirSync(models)
-  .filter(file => ~file.search(/^[^.].*js$/))
-  .forEach(file => require(resolve(models, file)))
+    .filter(file => ~file.search(/^[^.].*js$/))
+    .forEach(file => require(resolve(models, file)))
 
 export const database = app => {
-  mongoose.set('useCreateIndex', true)
+    mongoose.set('useCreateIndex', true)
 
-  mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
-
-  mongoose.connection.on('disconnected', () => {
     mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
-  })
-  mongoose.connection.on('error', err => {
-    console.error(err)
-  })
 
-  mongoose.connection.on('open', async () => {
-    console.log('Connected to MongoDB ', config.db)
+    mongoose.connection.on('disconnected', () => {
+        mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
+    })
+    mongoose.connection.on('error', err => {
+        console.error(err)
+    })
 
-    // const User = mongoose.model('User')
+    mongoose.connection.on('open', async () => {
+        console.log('Connected to MongoDB ', config.db)
 
-    // let user = new User({
-    //     username: 'admin',
-    //     password: '123456'
-    // })
+        const User = mongoose.model('User')
 
-    // await user.save()
-  })
+        const user = await User.findOne({ username: 'admin' })
+
+        if (!user) {
+            console.log('---------','没有admin')
+            let user = new User({
+                username: 'admin',
+                password: '123456'
+            })
+
+            await user.save()
+        }
+
+
+    })
 }
