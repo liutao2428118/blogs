ff9bbf12873c971936b9504b0170d35e31a12e26
tao.lt <liutao2428118@163.com>
2020-08-26 11:07:11 +0800

    代码风格修复

--- server/middlewares/database.js
+++ server/middlewares/database.js
@@ -6,38 +6,36 @@ import config from '../config'
 const models = resolve(__dirname, '../database/schema')
 
 fs.readdirSync(models)
-    .filter(file => ~file.search(/^[^.].*js$/))
-    .forEach(file => require(resolve(models, file)))
+  .filter(file => ~file.search(/^[^.].*js$/))
+  .forEach(file => require(resolve(models, file)))
 
 export const database = app => {
-    mongoose.set('useCreateIndex', true)
+  mongoose.set('useCreateIndex', true)
 
+  mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  mongoose.connection.on('disconnected', () => {
     mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
+  })
+  mongoose.connection.on('error', err => {
+    console.error(err)
+  })
 
-    mongoose.connection.on('disconnected', () => {
-        mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
-    })
-    mongoose.connection.on('error', err => {
-        console.error(err)
-    })
+  mongoose.connection.on('open', async () => {
+    console.log('Connected to MongoDB ', config.db)
 
-    mongoose.connection.on('open', async () => {
-        console.log('Connected to MongoDB ', config.db)
+    const User = mongoose.model('User')
 
-        const User = mongoose.model('User')
+    const user = await User.findOne({ username: 'admin' })
 
-        const user = await User.findOne({ username: 'admin' })
+    if (!user) {
+      console.log('---------', '没有admin')
+      const user = new User({
+        username: 'admin',
+        password: '123456'
+      })
 
-        if (!user) {
-            console.log('---------','没有admin')
-            let user = new User({
-                username: 'admin',
-                password: '123456'
-            })
-
-            await user.save()
-        }
-
-
-    })
+      await user.save()
+    }
+  })
 }
